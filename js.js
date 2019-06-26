d049c733dde45feec4c5566d05dd65fe8230a5e3
Anisha Shaji <753943@student.tor.loc>
2019-06-26 14:17:29 -0400

    2.15

--- js.js
+++ js.js
@@ -1,2 +1,12 @@
-function waitingfn(3000);
+function  waitingfn(timeinms){
+    const futuretime  =  date.now() = timeinms;
+    while (futuretime > date.now()){}
+
+
+
+ }
+ waitingfn(3000)
+console.log("function called has just ended");
+
+
     
